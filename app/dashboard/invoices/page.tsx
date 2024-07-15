5974153c7ee7f512e6afb537ac7d1e1274c602c1
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2024-07-15 12:17:24 +0300

    complete 11 chapter (adding search and pagination)

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,6 +4,8 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import Table from '@/app/ui/invoices/table';
+import { fetchInvoicesPages } from '@/app/lib/data';
+import Pagination from '@/app/ui/invoices/pagination';
 
 export default async function Page({
   searchParams
@@ -15,6 +17,8 @@ export default async function Page({
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -29,7 +33,7 @@ export default async function Page({
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+         <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,16 +3,16 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import React from 'react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    console.log(term);
-
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
@@ -20,7 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
