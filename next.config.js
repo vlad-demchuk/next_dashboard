c6e596a529296be1ee398dac41f285804b081c73
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2024-07-16 14:01:19 +0300

    versions uptick, complete 12 chapter (mutating data)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    ppr: 'incremental',
+    // To turn on caching from Next 14
+    // staleTimes: {
+    //   dynamic: 30
+    // }
+  },
+};
 
 module.exports = nextConfig;
